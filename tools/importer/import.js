18713b5120cfbeb8f94eb9d4d077f79a27b60b68
Vivek Kumar <vivek1@adobe.com>
2025-02-14 14:54:21 +0530

    importer.js added

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -1,22 +1,11 @@
-// export default {
-//     //document object-->represents HTML DOM-->contains structure of webpage
-//     transformDOM: ({ document }) => {
-//       const main = document.querySelector('main'); //main element selected--contains core content of webpage
-//       main.querySelector('header, footer').forEach(el => el.remove());
-//       return main; //returns main after removing the specified elements
-//     },
-//   };
-
-
-//for adding any class--> use .('header,footer, .class_name')
-
-
 export default {
     //document object-->represents HTML DOM-->contains structure of webpage
     transformDOM: ({ document }) => {
       const main = document.querySelector('main'); //main element selected--contains core content of webpage
-      // Simply return the main element without removing header or footer
-      return main; //returns main without removing header and footer
+      main.querySelector('header, footer').forEach(el => el.remove());
+      return main; //returns main after removing the specified elements
     },
   };
-  
+
+
+//for adding any class--> use .('header,footer, .class_name')
