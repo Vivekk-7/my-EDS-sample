e70497cbc71c9a2447f625cc6d0f682af6fdc20f
Vivek Kumar <vivek1@adobe.com>
2025-02-14 14:38:10 +0530

    import.js file modified

--- tools/importer/import.js
+++
@@ -1,7 +0,0 @@
-export default {
-    transformDOM: ({ document }) => {
-      const main = document.querySelector('main');
-      main.querySelector('header, footer, .disclaimer').forEach(el => el.remove());
-      return main;
-    },
-  };
